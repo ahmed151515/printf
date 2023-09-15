5d06e5e90d4accfc81360315f7c04b000dce28fc
ahmed arafa <ahmedarafa.200444@gmail.com>
2023-09-15 10:33:56 +0300

    set environment

---
+++ README.md
@@ -0,0 +1,4 @@
+**0x11. C - printf**
+made by
+	Ahmed Arafa
+	Ziad Mohamed
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#endif
