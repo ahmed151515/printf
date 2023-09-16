ee8e96ba5fb7ab0093fb41c15e61326a5efad3d5
Ziad Mohamed <ziadelgammal123@gmail.com>
2023-09-16 06:29:52 -0700

    commi

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - like printf
  * @format: srintg
--- function.c
+++ function.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_char - Prints a char
  * @types: List a of arguments
@@ -95,7 +96,6 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -137,49 +137,4 @@ int print_int(va_list types, char buffer[],
 	i++;
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
-}
-
-/**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
- */
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
-
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
-	n = va_arg(types, unsigned int);
-	m = 2147483648;
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
-
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
 }
